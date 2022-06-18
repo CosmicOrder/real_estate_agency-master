e0c97bdee601bf5400c52d4280f85d13ef33bdcf
CosmicOrder <Matrixxturn@yandex.ru>
2022-06-19 00:31:01 +0300

    feat: add db model for likes

--- property/models.py
+++ property/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
@@ -50,3 +51,17 @@ class Flat(models.Model):
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
+
+
+class Complaint(models.Model):
+    who_complaint = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Кто жаловался'
+    )
+    flat = models.ForeignKey(
+        Flat,
+        on_delete=models.CASCADE,
+        verbose_name='Квартира на которую пожаловались',
+    )
+    complaint_text = models.TextField('Текст жалобы', blank=True)
